aafb0000d1d6e7aafe4f32abd1a9c374f297b9c1
NotTom31 <thomasmezzie@gmail.com>
2023-03-09 21:36:33 -0800

    finished project 3

--- Project 3/scripts/projectileCalculations.js
+++ Project 3/scripts/projectileCalculations.js
@@ -8,12 +8,48 @@ function initialize(){
 
 function validateAngle(){
     var angleInput=document.getElementById("angle");
-    if(angleInput.value < 1|| angleInput > 90){
+    if(angleInput.value < 1|| angleInput.value > 90){
         alert("Angle value must be between 1 and 90")
-        angleInput.value="".
+        angleInput.value="";
     }
 }
 
 function validateVelocity(){
+    var velocityInput=document.getElementById("velocity");
+    if(velocityInput.value<1){
+        alert("Velocity value must greater than 0")
+        angleInput.value="";
+    }
+    else if (velocityInput.value>299792458){
+        alert("Too fast! The velocity value can not exceed[...]");
+        angleInput.value="";
+    }
+}
 
+function update(){
+    var angle=document.getElementById("angle").value;
+    var velocity=document.getElementById("velocity").value;
+    calculate(angle, velocity);
+}
+
+function calculate(angle, velocity){
+    var horizontalVelocity = velocity * Math.cos((angle * Math.PI)/180);
+    var verticalVelocity = velocity * Math.sin((angle * Math.PI)/180);
+    var tMaxHeight = verticalVelocity / 9.81;
+    var tLanding = 2 * tMaxHeight;
+
+    document.getElementById("height").innerHTML = calcHeight(verticalVelocity, tMaxHeight).toFixed(3);
+
+    document.getElementById("distance").innerHTML = calcDistance(horizontalVelocity, tLanding).toFixed(3);
+
+}
+
+function calcDistance(horizontalVelocity, time){
+    var distance = horizontalVelocity * time;
+    return distance;
+}
+
+function calcHeight(verticalVelocity, time){
+    var height = (verticalVelocity * time) - (0.5 * 9.81 * time * time);
+    return height;
 }
